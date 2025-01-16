e17acea4aa9eed840bf773912fb003d2750bb771
Michał Czapliński <czaplinskim@icloud.com>
2025-01-16 20:01:44 +0100

    Fixes

--- resources/views/sections/faq.blade.php
+++ resources/views/sections/faq.blade.php
@@ -13,12 +13,12 @@ $faq_items = json_decode(get_theme_mod('items', '[]'), true) ?: null;
             <div class="animate-text flex flex-col max-w-xl mx-auto mb-10 w-full divide-y-[1.5px] divide-stroke border-stroke border-y-[1.5px] " id="faq-accordion">
                 @if(isset($faq_items))
                 @foreach ($faq_items as $index => $item)
-                    <div class="faq-accordion-item px-3.5 lg:px-8 cursor-pointer">
-                        <div class="flex justify-between items-center py-6 faq-accordion-header" id="accordion-title-{{ $index }}">
+                    <div class="faq-accordion-item px-2 md:px-3.5 lg:px-8 cursor-pointer">
+                        <div class="flex justify-between items-baseline py-6 faq-accordion-header gap-x-1" id="accordion-title-{{ $index }}">
                                 <h6 class="uppercase text-white font-medium font-sans text-[26px]">{{ $item['question'] }}</h6>
-                                <x-i-arrow height="15" fill="white"></x-i-arrow>
+                                <span><x-i-arrow height="15" fill="white"></x-i-arrow></span>
                         </div>
-                        <div class="pt-2 pb-8">
+                        <div class="pt-2 pb-8  pr-8">
                             <p class="font-sans font-light text-2xl text-white leading-snug">
                                 {!! $item['answer'] !!}
                             </p>
