85fdc8ea4839ee4f924af62894a9a5ee0fc320c5
Michał Czapliński <czaplinskim@icloud.com>
2025-01-16 12:03:45 +0100

    Lang check

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -12,6 +12,9 @@
         $yearArray = str_split($year);
 
     }
+
+    $current_lang = isset($_SERVER['HTTP_X_GT_LANG']) ? $_SERVER['HTTP_X_GT_LANG'] : '';
+    dd($current_lang);
 @endphp
 
 <section id="about" class="bg-main relative overflow-hidden">    
